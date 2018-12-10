a0a9b404d7529ef30b7fb65d0d2812636fbea197
千夜 <1342746626@qq.com>
2018-12-10 12:17:29 +0800

    初次提交

---
+++ src/main/java/com/lz/palmscore/PalmscoreApplication.java
@@ -0,0 +1,12 @@
+package com.lz.palmscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalmscoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalmscoreApplication.class, args);
+    }
+}
