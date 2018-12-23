0947037059babc618c0659ac85d2705748158496
千夜 <1342746626@qq.com>
2018-12-23 17:40:08 +0800

    显示总评分的评分项精度保留2位

--- src/main/java/com/lz/palmscore/service/impl/PlayerServiceImpl.java
+++ src/main/java/com/lz/palmscore/service/impl/PlayerServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -186,6 +187,10 @@ public class PlayerServiceImpl implements PlayerService {
         for (ScoreItem scoreItem : scoreItemList) {
             PlayerScoreitem ps = new PlayerScoreitem();
             Double score = scoreItem.getRate() / totalScore;
+
+            BigDecimal b = new BigDecimal(score);
+            score = b.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+
             ps.setScore(score);
 
             playerScoreitems.add(ps);
